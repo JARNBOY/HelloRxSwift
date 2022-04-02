9e0f545642399b712d14c910248dd35adf754801
Papon <tattoo197@outlook.com>
2022-04-02 12:27:01 +0700

    understand type in observable

--- HelloSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloSwift/RxSwiftPlayground.playground/Contents.swift
@@ -2,4 +2,9 @@ import UIKit
 import RxSwift
 
 
-_ = Observable.from([1,2,3,4,5])
+let observable = Observable.just(1)
+let observable2 = Observable.of(1,2,3,4,5)
+let observable3 = Observable.of([1,2,3,4,5])
+let observable4 = Observable.from([1,2,3,4,5])
+
+
