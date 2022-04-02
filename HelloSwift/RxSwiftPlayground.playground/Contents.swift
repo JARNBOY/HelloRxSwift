4d5b143ea0547c0941b6b5ca1760de404a14d23e
Papon <tattoo197@outlook.com>
2022-04-02 12:54:48 +0700

    Disposing and Terminating

--- HelloSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloSwift/RxSwiftPlayground.playground/Contents.swift
@@ -1,27 +1,26 @@
 import UIKit
 import RxSwift
 
+let disposeBag = DisposeBag()
 
-let observable = Observable.just(1)
-let observable2 = Observable.of(1,2,3,4,5)
-let observable3 = Observable.of([1,2,3,4,5])
-let observable4 = Observable.from([1,2,3,4,5])
+Observable.of("A","B","C")
+    .subscribe {
+        print($0)
+    }.disposed(by: disposeBag)
 
-observable4.subscribe { event in
-    if let element = event.element{
-        print(element)
-    }
-    
-}
 
-observable3.subscribe { event in
-    if let element = event.element{
-        print(element)
-    }
-}
-
-let subscribtion4 = observable4.subscribe (onNext: { element in
-    print(element)
-})
-subscribtion4.dispose()
+Observable<String>.create { observer in
+    observer.onNext("A")
+    observer.onCompleted()
+    observer.onNext("?")
+    return Disposables.create()
+}.subscribe {
+    print($0)
+} onError: {
+    print($0)
+} onCompleted: {
+    print("Completed")
+} onDisposed: {
+    print("Disposed")
+}.disposed(by: disposeBag)
 
