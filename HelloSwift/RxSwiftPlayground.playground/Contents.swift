c908bb5ec94dc1c6160acda1613dce4192025791
Papon <tattoo197@outlook.com>
2022-04-02 15:47:48 +0700

    ReplaySubject

--- HelloSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloSwift/RxSwiftPlayground.playground/Contents.swift
@@ -3,13 +3,20 @@ import RxSwift
 
 let disposeBag = DisposeBag()
 
-let subject =  BehaviorSubject(value: "initial Value")
-
-subject.onNext("Last Issue")
-subject.subscribe{ event in
-    print(event)
-}
+let subject =  ReplaySubject<String>.create(bufferSize: 2)
 
 subject.onNext("Issue 1")
 subject.onNext("Issue 2")
 subject.onNext("Issue 3")
+subject.subscribe{ event in
+    print(event)
+}
+
+subject.onNext("Issue 4")
+subject.onNext("Issue 5")
+subject.onNext("Issue 6")
+
+print("Subcribtion 2")
+subject.subscribe{ event in
+    print(event)
+}
