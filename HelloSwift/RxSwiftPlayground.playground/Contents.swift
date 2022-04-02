204d9881e868bad4bcf667f50cdf83fa44e52458
Papon <tattoo197@outlook.com>
2022-04-02 12:39:37 +0700

    implementing subscriptions

--- HelloSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloSwift/RxSwiftPlayground.playground/Contents.swift
@@ -7,4 +7,20 @@ let observable2 = Observable.of(1,2,3,4,5)
 let observable3 = Observable.of([1,2,3,4,5])
 let observable4 = Observable.from([1,2,3,4,5])
 
+observable4.subscribe { event in
+    if let element = event.element{
+        print(element)
+    }
+    
+}
+
+observable3.subscribe { event in
+    if let element = event.element{
+        print(element)
+    }
+}
+
+observable4.subscribe (onNext: { element in
+    print(element)
+})
 
