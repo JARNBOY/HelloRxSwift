e9cc69b89299c6f9580c8565ac4da65236dacdd2
Papon <tattoo197@outlook.com>
2022-04-03 19:45:59 +0700

    Filter -> skip

--- HelloSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloSwift/RxSwiftPlayground.playground/Contents.swift
@@ -5,8 +5,8 @@ import RxCocoa
 //let strikes = PublishSubject<String>()
 let disposeBag = DisposeBag()
 
-Observable.of(1,2,3,4,5,6,7)
-    .filter { $0 % 2 == 0}
+Observable.of("A","B","C","D","E","F")
+    .skip(3)
     .subscribe(onNext: {
         print($0)
     }).disposed(by: disposeBag)
