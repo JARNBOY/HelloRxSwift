5d496d51b3b8cc0c256343b7396bfbd00031dc49
Papon <tattoo197@outlook.com>
2022-04-03 20:01:07 +0700

    Filter -> skipUntil

--- HelloSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloSwift/RxSwiftPlayground.playground/Contents.swift
@@ -2,12 +2,19 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-//let strikes = PublishSubject<String>()
+let subject = PublishSubject<String>()
+let trigger = PublishSubject<String>()
 let disposeBag = DisposeBag()
 
-Observable.of(2,2,3,4,4)
-    .skipWhile({  $0 % 2 == 0})
+subject.skipUntil(trigger)
     .subscribe(onNext: {
         print($0)
     }).disposed(by: disposeBag)
+
+subject.onNext("A")
+subject.onNext("B")
+
+trigger.onNext("X")
+
+subject.onNext("C")
 
