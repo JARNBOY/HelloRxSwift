74a7dd11d0e2282ae917cab9c7ef182bec5abdf4
Papon <tattoo197@outlook.com>
2022-04-03 20:13:05 +0700

    Filter -> takUntil

--- HelloSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloSwift/RxSwiftPlayground.playground/Contents.swift
@@ -2,25 +2,21 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-//let subject = PublishSubject<String>()
+let subject = PublishSubject<String>()
+let trigger = PublishSubject<String>()
 let disposeBag = DisposeBag()
 
-Observable.of(2,4,5,6)
-    .takeWhile{
-        return $0 % 2 == 0
-    }.subscribe(onNext: {
+
+
+subject.takeUntil(trigger)
+    .subscribe(onNext: {
         print($0)
     }).disposed(by: disposeBag)
 
-//subject.skipUntil(trigger)
-//    .subscribe(onNext: {
-//        print($0)
-//    }).disposed(by: disposeBag)
-//
-//subject.onNext("A")
-//subject.onNext("B")
-//
-//trigger.onNext("X")
-//
-//subject.onNext("C")
+subject.onNext("1")
+subject.onNext("2")
+
+trigger.onNext("X")
+
+subject.onNext("3")
 
