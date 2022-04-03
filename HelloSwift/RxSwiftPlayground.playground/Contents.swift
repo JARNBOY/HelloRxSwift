36942bd4ca3cd0f0bd68fcd9a7421a47b262238d
Papon <tattoo197@outlook.com>
2022-04-03 20:05:36 +0700

    Filter -> take

--- HelloSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloSwift/RxSwiftPlayground.playground/Contents.swift
@@ -2,19 +2,24 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-let subject = PublishSubject<String>()
-let trigger = PublishSubject<String>()
+//let subject = PublishSubject<String>()
 let disposeBag = DisposeBag()
 
-subject.skipUntil(trigger)
+Observable.of(1,2,3,4,5,6)
+    .take(3)
     .subscribe(onNext: {
         print($0)
     }).disposed(by: disposeBag)
 
-subject.onNext("A")
-subject.onNext("B")
-
-trigger.onNext("X")
-
-subject.onNext("C")
+//subject.skipUntil(trigger)
+//    .subscribe(onNext: {
+//        print($0)
+//    }).disposed(by: disposeBag)
+//
+//subject.onNext("A")
+//subject.onNext("B")
+//
+//trigger.onNext("X")
+//
+//subject.onNext("C")
 
