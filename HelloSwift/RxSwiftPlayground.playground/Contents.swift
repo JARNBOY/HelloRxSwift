2084bf9ecae9148973bf40b7c87846e828fd6bf0
Papon <tattoo197@outlook.com>
2022-04-06 22:22:49 +0700

    Transforming - Flat Map

--- HelloSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloSwift/RxSwiftPlayground.playground/Contents.swift
@@ -4,12 +4,25 @@ import RxCocoa
 
 let disposeBag = DisposeBag()
 
-Observable.of(1,2,3,4,5)
-    .map({
-        return $0 * 2
-    })
-    .subscribe(onNext:{
+struct Student{
+    var score: BehaviorRelay<Int>
+}
+
+let john = Student(score: BehaviorRelay(value: 75))
+let mary = Student(score: BehaviorRelay(value: 90))
+
+let student = PublishSubject<Student>()
+
+student.asObserver()
+    .flatMap {
+        $0.score.asObservable()
+    }.subscribe(onNext:{
         print($0)
     }).disposed(by: disposeBag)
+
+student.onNext(john)
+john.score.accept(100)
+
+student.onNext(mary)
 
 
