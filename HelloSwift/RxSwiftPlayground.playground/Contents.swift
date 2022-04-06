cd46a3611c8fa0baa87639bb0217b16c2678589b
Papon <tattoo197@outlook.com>
2022-04-06 22:07:48 +0700

    Transferming - To Array

--- HelloSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloSwift/RxSwiftPlayground.playground/Contents.swift
@@ -2,21 +2,12 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-let subject = PublishSubject<String>()
-let trigger = PublishSubject<String>()
 let disposeBag = DisposeBag()
 
-
-
-subject.takeUntil(trigger)
-    .subscribe(onNext: {
+Observable.of(1,2,3,4,5)
+    .toArray()
+    .subscribe(onNext:{
         print($0)
     }).disposed(by: disposeBag)
 
-subject.onNext("1")
-subject.onNext("2")
-
-trigger.onNext("X")
-
-subject.onNext("3")
 
