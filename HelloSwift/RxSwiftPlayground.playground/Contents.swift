af9b0fb541ce8a32f28781052b8b1a90f7b0593e
Papon <tattoo197@outlook.com>
2022-04-09 14:54:15 +0700

    Combining - Concat

--- HelloSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloSwift/RxSwiftPlayground.playground/Contents.swift
@@ -4,16 +4,11 @@ import RxCocoa
 
 let disposeBag = DisposeBag()
 
-struct Student{
-    var score: BehaviorRelay<Int>
-}
 
+let first = Observable.of(1,2,3)
+let second = Observable.of(4,5,6)
 
-let student = PublishSubject<Student>()
-
-let numbers = Observable.of(2,3,4)
-
-let observable = numbers.startWith(1)
+let observable = Observable.concat([first,second])
 observable
     .subscribe(onNext:{
         print($0)
