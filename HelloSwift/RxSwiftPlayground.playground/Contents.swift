54bf57ae0d4d19bace83765133510ca19de6d743
Papon <tattoo197@outlook.com>
2022-04-09 14:50:50 +0700

    Combining - Starts With

--- HelloSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloSwift/RxSwiftPlayground.playground/Contents.swift
@@ -8,21 +8,15 @@ struct Student{
     var score: BehaviorRelay<Int>
 }
 
-let john = Student(score: BehaviorRelay(value: 75))
-let mary = Student(score: BehaviorRelay(value: 90))
 
 let student = PublishSubject<Student>()
 
-student.asObserver()
-    .flatMapLatest {
-        $0.score.asObservable()
-    }.subscribe(onNext:{
+let numbers = Observable.of(2,3,4)
+
+let observable = numbers.startWith(1)
+observable
+    .subscribe(onNext:{
         print($0)
     }).disposed(by: disposeBag)
 
-student.onNext(john)
-john.score.accept(100)
-john.score.accept(50)
-student.onNext(mary)
-john.score.accept(40)
 
