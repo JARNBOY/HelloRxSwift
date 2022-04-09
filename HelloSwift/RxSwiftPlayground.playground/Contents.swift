4dd6e888234b294c00d45d8bdc03c3b29259095a
Papon <tattoo197@outlook.com>
2022-04-09 14:59:20 +0700

    Combining - Merge

--- HelloSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloSwift/RxSwiftPlayground.playground/Contents.swift
@@ -5,13 +5,17 @@ import RxCocoa
 let disposeBag = DisposeBag()
 
 
-let first = Observable.of(1,2,3)
-let second = Observable.of(4,5,6)
+let left = PublishSubject<Int>()
+let right = PublishSubject<Int>()
 
-let observable = Observable.concat([first,second])
-observable
-    .subscribe(onNext:{
+let source = Observable.of(left.asObserver(),right.asObserver())
+let observable = source.merge()
+observable.subscribe(onNext:{
         print($0)
-    }).disposed(by: disposeBag)
+}).disposed(by: disposeBag)
 
-
+left.onNext(5)
+left.onNext(3)
+right.onNext(2)
+right.onNext(1)
+left.onNext(99)
